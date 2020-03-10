bb94feef5146ebb569f01bbc789ecde37a1f3769
hansenjl <jenn.leigh.hansen@gmail.com>
2020-03-10 09:56:13 -0700

    set up db with users and students

--- backend/db/migrate/20200310164731_create_students.rb
+++ backend/db/migrate/20200310164731_create_students.rb
@@ -4,6 +4,7 @@ class CreateStudents < ActiveRecord::Migration[5.1]
       t.string :first_name
       t.string :last_name
       t.string :profile_url
+      t.belongs_to :user
 
       t.timestamps
     end
